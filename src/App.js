4489ab3d4f8aaccd68f7b8af764394d772956cca
khargr14 <kay_hargrove@yahoo.com>
2022-01-29 12:01:23 -0500

    add button style

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button 
+            startIcon={<SaveIcon />}
+            size="large"
+            variant="contained" 
+            color="secondary">
+          Hello World
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
